8092dd3f21f11a983cef2fce3ff775d4849a1211
AlejandroGlm <alejandrogaspar89@gmail.com>
2024-12-06 18:59:19 -0600

    menu principal

--- src/screens/Inicio.jsx
+++ src/screens/Inicio.jsx
@@ -1,4 +1,5 @@
-import { DTable, Footer, Menu, Navbar, Title } from "../components";
+import { Footer, Menu, Navbar, Title } from "../components";
+import { Link } from "react-router-dom";
 
 export const Inicio = () => {
     return (
@@ -8,12 +9,46 @@ export const Inicio = () => {
             <div className="content-wrapper">
                 <Title title="Bienvenido(s)" breadcrums={["Menú principal"]} />
                 <section className="content">
-
-                    
-
+                    <div className="container-fluid">
+                        <div className="row">
+                            {/* Card Mobiliario */}
+                            <div className="col-md-3">
+                                <div className="card">
+                                    <div className="card-body text-center">
+                                        <i className="fas fa-desktop fa-3x mb-3"></i>
+                                        <h5 className="card-title">Mobiliario</h5>
+                                        <p className="card-text">Administra los muebles y equipos.</p>
+                                        <Link to="/mobiliario" className="btn btn-primary">Ir</Link>
+                                    </div>
+                                </div>
+                            </div>
+                            {/* Card Ubicaciones */}
+                            <div className="col-md-3">
+                                <div className="card">
+                                    <div className="card-body text-center">
+                                        <i className="fas fa-building fa-3x mb-3"></i>
+                                        <h5 className="card-title">Ubicaciones</h5>
+                                        <p className="card-text">Consulta y gestiona las ubicaciones.</p>
+                                        <Link to="/ubicaciones" className="btn btn-primary">Ir</Link>
+                                    </div>
+                                </div>
+                            </div>
+                            {/* Card Reportes */}
+                            <div className="col-md-3">
+                                <div className="card">
+                                    <div className="card-body text-center">
+                                        <i className="fas fa-chart-pie fa-3x mb-3"></i>
+                                        <h5 className="card-title">Reportes</h5>
+                                        <p className="card-text">Genera informes y reportes.</p>
+                                        <Link to="/informes" className="btn btn-primary">Ir</Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </section>
             </div>
             <Footer />
         </>
-    )
-}
+    );
+};
